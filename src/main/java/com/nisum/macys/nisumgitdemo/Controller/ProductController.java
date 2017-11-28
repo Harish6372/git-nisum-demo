48d8f92a7e240eae6107fcdda81a77e16e0ba16f
sureharish <sureharish63@gmail.com>
2017-11-28 11:51:06 +0530

    hjsdghj

--- src/main/java/com/nisum/macys/nisumgitdemo/Controller/ProductController.java
+++ src/main/java/com/nisum/macys/nisumgitdemo/Controller/ProductController.java
@@ -3,8 +3,8 @@ package com.nisum.macys.nisumgitdemo.Controller;
 public class ProductController {
 	public String getProduct()
 	{
-		System.out.println("remote"):
-		return " one oproduct";
+		
+		return " ssss oproduct";
 	}
 
 }
