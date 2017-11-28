ba48bc2ae62117b6a86b49d6c36f76f9f13f1eaa
sureharish <sureharish63@gmail.com>
2017-11-28 12:01:33 +0530

    mbdsadmsans

--- src/main/java/com/nisum/macys/nisumgitdemo/NisumgitdemoApplication.java
+++ src/main/java/com/nisum/macys/nisumgitdemo/NisumgitdemoApplication.java
@@ -9,6 +9,9 @@ public class NisumgitdemoApplication {
 	public static void main(String[] args) {
 		System.out.println("sd,jfhjds");
 		System.out.println("harish");
+		System.out.println("local1");
+		System.out.println("ssss");
+		System.out.println("ssss");
 		SpringApplication.run(NisumgitdemoApplication.class, args);
 	}
 }
