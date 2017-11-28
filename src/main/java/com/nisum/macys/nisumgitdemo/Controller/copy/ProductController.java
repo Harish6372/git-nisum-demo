bbd275d1d8ae8e620ea47fa1a6405fefb7ebf677
sureharish <sureharish63@gmail.com>
2017-11-28 11:46:50 +0530

    jasjds

--- src/main/java/com/nisum/macys/nisumgitdemo/Controller/copy/ProductController.java
+++ src/main/java/com/nisum/macys/nisumgitdemo/Controller/copy/ProductController.java
@@ -1,8 +1,13 @@
 package com.nisum.macys.nisumgitdemo.Controller.copy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ProductController {
+	Logger logger=LoggerFactory.getLogger(this.getClass());
 	public String getProduct()
 	{
+		logger.debug("entered into"+this.getClass());
 		return "single product";
 	}
 
