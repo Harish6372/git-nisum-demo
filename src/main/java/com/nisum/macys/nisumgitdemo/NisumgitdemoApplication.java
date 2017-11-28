8a604d35f47af03532124da84ac42f5576865c46
Harish6372 <32665763+Harish6372@users.noreply.github.com>
2017-11-28 11:55:13 +0530

    dsd

--- src/main/java/com/nisum/macys/nisumgitdemo/NisumgitdemoApplication.java
+++ src/main/java/com/nisum/macys/nisumgitdemo/NisumgitdemoApplication.java
@@ -8,6 +8,7 @@ public class NisumgitdemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("sd,jfhjds");
+		System.out.println("sd,jfhjdsssss");
 		SpringApplication.run(NisumgitdemoApplication.class, args);
 	}
 }
