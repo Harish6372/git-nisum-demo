11eba895d08bb65505aeb59d2abdcdc75a6ef48c
sureharish <sureharish63@gmail.com>
2017-11-28 11:33:59 +0530

    first commit

---
+++ src/main/java/com/nisum/macys/nisumgitdemo/NisumgitdemoApplication.java
@@ -0,0 +1,12 @@
+package com.nisum.macys.nisumgitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NisumgitdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NisumgitdemoApplication.class, args);
+	}
+}
