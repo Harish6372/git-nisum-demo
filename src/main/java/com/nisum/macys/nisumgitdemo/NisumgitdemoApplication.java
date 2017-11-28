ccaeb2d83ed7ffd26fd4906850ffc0178fe228df
Harish6372 <32665763+Harish6372@users.noreply.github.com>
2017-11-28 12:00:32 +0530

    dffd

--- src/main/java/com/nisum/macys/nisumgitdemo/NisumgitdemoApplication.java
+++ src/main/java/com/nisum/macys/nisumgitdemo/NisumgitdemoApplication.java
@@ -7,9 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NisumgitdemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("sd,jfhjds");
+		System.out.println("sd,jfhjdsmhamsad");
 		System.out.println("harish");
 		System.out.println("remote");
+		System.out.println("remote1");
+		System.out.println("remote2");
 		SpringApplication.run(NisumgitdemoApplication.class, args);
 	}
 }
